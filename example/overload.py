4cdb65363299ceffb4f08ffe3c51efc062ad4643
Thomas Lettau <thomas@shadowice.org>
2019-03-25 14:08:17 +0100

    Added example of overloaded init

---
+++ example/overload.py
@@ -0,0 +1,51 @@
+from cornichon import load_all, save_all
+
+
+class SubClass:
+    def __init__(self):
+        self.value = 1
+
+    save_all = save_all
+    load_all = load_all
+
+
+class SubClass2:
+    def __init__(self):
+        self.value = 1
+
+    save_all = save_all
+    load_all = load_all
+
+
+class TestClass:
+    def __init__(self):
+        self.d = SubClass()
+        self.d2 = SubClass2()
+
+        self.a = [1, 2, 3]
+        self.b = 'Alex ist ein toller Hecht'
+        self.c = 1
+
+    @classmethod
+    def from_file(cls, filename):
+        TC = cls()
+        TC.load_all(filename)
+        return TC
+
+    def print_all(self):
+        print(self.__dict__)
+
+    save_all = save_all
+    load_all = load_all
+
+
+if __name__ == "__main__":
+    filename = 'data/pickle'
+
+    tc = TestClass()
+    tc.print_all()
+    tc.save_all(filename)
+
+    tc2 = TestClass.from_file(filename)
+    tc2.load_all(filename)
+    tc2.print_all()
